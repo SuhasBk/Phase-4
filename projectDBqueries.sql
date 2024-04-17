85e0c086abc83e1a45b71c8dfc091f22ad399965
Suhas Bk <kowligi1998@gmail.com>
2024-04-17 17:15:09 -0500

    queries + advertiserID FK

--- projectDBqueries.sql
+++ projectDBqueries.sql
@@ -45,7 +45,7 @@ HAVING
 ORDER BY VideoCount desc;
 
 
--- 3) Get the top 3 advertisers and the total number of ads posted by them which have no more than 5 ads across all episodes and whose total revenue exceeds 10000.
+-- 3) Get the top 3 advertisers and the total number of ads posted by them which have no more than 5 ads across all episodes and whose total revenue exceeds 5000.
 
 SELECT
     adv.Name,
@@ -57,7 +57,7 @@ JOIN
 JOIN
     S24_S003_T7_DISPLAYS d ON d.AdID = ad.AdID AND d.AdvertiserID = adv.AdvertiserID
 WHERE
-    adv.revenue > 10000
+    adv.revenue > 5000
 GROUP BY
     adv.Name,
     adv.AdvertiserID
@@ -112,3 +112,38 @@ GROUP BY
     ROLLUP(artist_details.Lname, artist_details.Fname, user_details.Region, P.Name)
 ORDER BY
     artist_details.Lname, artist_details.Fname, user_details.Region, P.Name;
+
+
+-- 6) Get the advertiser name whose ads appear in every episode of every podcast
+-- (Division operation)
+
+SELECT d.AdID, d.AdvertiserID
+FROM S24_S003_T7_ADS d
+WHERE NOT EXISTS (
+    SELECT e.EpisodeID, e.PodcastID
+    FROM S24_S003_T7_EPISODE e
+    WHERE NOT EXISTS (
+        SELECT AdID
+        FROM S24_S003_T7_DISPLAYS ds
+        WHERE ds.EpisodeID = e.EpisodeID
+          AND ds.PodcastID = e.PodcastID
+          AND ds.AdID = d.AdID
+          AND ds.AdvertiserID = d.AdvertiserID
+    )
+);
+
+-- 7) Within each genre, calculate the ranking of episodes based on the number of likes it has recieved.
+
+SELECT
+    e.EpisodeID,
+    e.PodcastID,
+    e.Likes,
+    p.Name AS PodcastName,
+    g.Name AS GenreName,
+    ROW_NUMBER() OVER (PARTITION BY g.Name ORDER BY e.Likes DESC) AS episode_rank_in_genre
+FROM
+    S24_S003_T7_EPISODE e
+JOIN
+    S24_S003_T7_PODCAST p ON e.PodcastID = p.PodcastID
+JOIN
+    S24_S003_T7_GENRE g ON p.GID = g.GID;
